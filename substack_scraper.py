8cabe9ad376fc72d8a6d84ab38a848e28525e1d1
timf34 <farrelti@tcd.ie>
2023-06-30 02:38:20 +0100

    quick improvements

--- substack_scraper.py
+++ substack_scraper.py
@@ -1,9 +1,7 @@
-from urllib.parse import urlparse
-import requests
 from bs4 import BeautifulSoup
 import html2text
 import requests
-from typing import Any, Dict, List, Optional, Tuple
+from typing import Any, Dict, List, Tuple
 from xml.etree import ElementTree as ET
 
 
@@ -20,7 +18,7 @@ class SubstackScraper:
         response = requests.get(sitemap_url)
 
         # Check if request was successful
-        if response.status_code == 200:
+        if response.ok:
             root = ET.fromstring(response.content)
             urls = [element.text for element in root.iter('{http://www.sitemaps.org/schemas/sitemap/0.9}loc')]
             urls = self.filter_urls(urls, self.keywords)
@@ -37,11 +35,12 @@ class SubstackScraper:
         return [url for url in urls if all(keyword not in url for keyword in keywords)]
 
     @staticmethod
-    def _substack_reader(soup: BeautifulSoup) -> Tuple[str, Dict[str, str]]:
+    def _substack_reader(soup: BeautifulSoup, url: str) -> Tuple[str, Dict[str, str]]:
         metadata = {
             "Title of this Substack post": soup.select_one("h1.post-title").text,
             "Subtitle": soup.select_one("h3.subtitle").text,
             "Author": soup.select_one("a.navbar-title-link").text,
+            "URL": url
         }
         text = soup.select_one("div.available-content").getText()
         return text, metadata
@@ -49,20 +48,26 @@ class SubstackScraper:
     def extract_post_content(self, url: str):
         try:
             page = requests.get(url, headers=None)
-            text = html2text.html2text(page.text)  # This prints it more nicely than bs4
             soup = BeautifulSoup(page.content, "html.parser")
-            metadata = {"URL": url}
-            _, metadata = self._substack_reader(soup)  # Note: _ here might actually be more parseable than the text above! But just leaving as is for now
-            metadata.update(metadata)
+            text = html2text.html2text(str(soup.select_one("div.available-content")))  # This prints it more nicely than bs4
+            _, metadata = self._substack_reader(soup, url)  # Note: _ here might actually be more parseable than the text above! But just leaving as is for now
             print(text)
             print(metadata)
         except Exception as e:
             raise ValueError(f"Error fetching page: {e}") from e
 
+
+# This can inherit from SubstackScraper down the line, but just keep it separate for now
+# Try log in using selenium and then scrape the premium content
+class PremiumSubstackScraper:
+    def __init__(self):
+        pass
+
+
 def main():
     scraper = SubstackScraper('https://ava.substack.com/')
     print(scraper.get_all_posts())
-    # scraper.extract_post_content('https://ava.substack.com/making-and-keeping-friends')
+    scraper.extract_post_content('https://ava.substack.com/making-and-keeping-friends')
 
 
 if __name__ == "__main__":
