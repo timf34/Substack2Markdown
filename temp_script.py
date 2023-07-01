2d8cdb3eeb4aca3fefa11e74f6c6bb8babfac997
timf34 <farrelti@tcd.ie>
2023-07-02 00:02:54 +0100

    markdown parsing working:)

--- temp_script.py
+++ temp_script.py
@@ -1,58 +1,22 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
-from time import sleep
+from bs4 import BeautifulSoup
+import html2text
+import os
 
-# Setup webdriver
-driver = webdriver.Edge(EdgeChromiumDriverManager().install())
-# driver = webdriver.Chrome(ChromeDriverManager().install())
+f_path: str = "data/ava/taste.html"
 
-# Navigate to the login page
-driver.get("https://substack.com/sign-in")
+with open(f_path, "r", encoding="utf-8") as f:
+    html = f.read()
 
-
-# First find the 'sign in with password' hyperlink and click it
-signin_with_password = driver.find_element(By.XPATH, "//a[@class='login-option substack-login__login-option']")
-signin_with_password.click()
-
-# The page may take some time to load the new fields after you click the link, so let's wait for a bit.
-sleep(3)
-
-# Find the email and password fields by their 'name' attribute.
-email = driver.find_element(By.NAME, "email")
-password = driver.find_element(By.NAME, "password")
-
-# Input your email and password into the fields.
-email.send_keys("farrelti@tcd.ie")
-password.send_keys("shopkeeper2")
-
-# Find the submit button and click it.
-submit = driver.find_element(By.XPATH, "//*[@id=\"substack-login\"]/div[2]/div[2]/form/div[3]/button")
-submit.click()
-
-sleep(5)
-
-driver.get("https://ava.substack.com/p/the-right-conversations")
-
-# Do some actions...
-input("Press enter to continue...")
-# Continue with the rest of your actions...
+soup = BeautifulSoup(html, "html.parser")
+div_content = str(soup.find('div', {'class': 'available-content'}))
 
 
-# Find the email and password fields. You'll need to replace 'email_field' and 'password_field'
-# with the actual names or ids of the fields.
-# email = driver.find_element(By.ID, "email_field")
-# password = driver.find_element(By.ID, "password_field")
-#
-# # Input your email and password into the fields
-# email.send_keys("YOUR_EMAIL")
-# password.send_keys("YOUR_PASSWORD")
-#
-# # Find the submit button and click it. Replace 'submit_button' with the actual id or name of the button.
-# submit = driver.find_element(By.ID, "submit_button")
-# submit.click()
-#
-# # Wait for a while to let the page load or check until a certain condition is met.
-# sleep(5)
-#
-# # Once login is successful, navigate to the next page
+h = html2text.HTML2Text()
+h.ignore_links = False
+h.body_width = 0  # Disable line wrapping
+md = h.handle(div_content)
+
+print(md)
+
+with open("test.md", 'w', encoding='utf-8') as f:
+    f.write(md)
