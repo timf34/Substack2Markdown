78f7d743033c0a8ba9d42196d81de43592b6da4f
timf34 <farrelti@tcd.ie>
2023-07-02 01:41:08 +0100

    big sexy refactoring

--- substack_scraper.py
+++ substack_scraper.py
@@ -1,8 +1,10 @@
+import os
+from typing import Dict, List, Tuple
+from time import sleep
+
 from bs4 import BeautifulSoup
 import html2text
-import os
 import requests
-from typing import Dict, List, Tuple
 from xml.etree import ElementTree as ET
 from tqdm import tqdm
 
@@ -10,18 +12,29 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.edge.options import Options as EdgeOptions
-from time import sleep
 
-class SubstackScraper:
-    def __init__(self, base_url: str):
-        self.base_url: str = base_url
-        self.keywords: List[str] = ["about", "archive", "podcast"]  # Keywords to filter out unwanted URLs
+# TODO: Create the savdir based on the name of the author unless specified otherwise
+
+
+class BaseSubstackScraper:
+    def __init__(self, base_substack_url: str, savdir: str):
+        if not base_substack_url.endswith("/"):
+            base_substack_url += "/"
+        self.base_substack_url: str = base_substack_url
+
+        # Check if the save_dir exists
+        if not os.path.exists(savdir):
+            os.makedirs(savdir)
+            print(f"Created directory {savdir}")
+        self.save_dir: str = savdir
+        self.keywords: List[str] = ["about", "archive", "podcast"]
+        self.post_urls: List[str] = self.get_all_post_urls()
 
     def get_all_post_urls(self) -> List[str]:
         """
         This method reads the sitemap.xml file and returns a list of all the URLs in the file
         """
-        sitemap_url = f"{self.base_url}sitemap.xml"
+        sitemap_url = f"{self.base_substack_url}sitemap.xml"
         response = requests.get(sitemap_url)
 
         if response.ok:
@@ -41,39 +54,95 @@ class SubstackScraper:
         return [url for url in urls if all(keyword not in url for keyword in keywords)]
 
     @staticmethod
-    def _substack_reader(soup: BeautifulSoup, url: str) -> Tuple[str, Dict[str, str]]:
+    def html_to_md(html_content: str) -> str:
         """
-        This method extracts the content of a Substack post and returns it as a string
+        This method converts HTML to Markdown
         """
-        metadata = {
-            "Title of this Substack post": soup.select_one("h1.post-title").text,
-            "Subtitle": soup.select_one("h3.subtitle").text,
-            "Author": soup.select_one("a.navbar-title-link").text,
-            "URL": url
-        }
-        text = soup.select_one("div.available-content").getText()
-        return text, metadata
+        if not isinstance(html_content, str):
+            raise ValueError("html_content must be a string")
+        h = html2text.HTML2Text()
+        h.ignore_links = False
+        h.body_width = 0
+        return h.handle(html_content)
 
-    def extract_post_content(self, url: str) -> None:
+    @staticmethod
+    def save_to_file(filepath: str, content: str) -> None:
         """
-        This method extracts the content of a Substack post and prints it to the console
+        This method saves content to a file. Can be used to save HTML or Markdown
+        """
+        if not isinstance(filepath, str):
+            raise ValueError("filepath must be a string")
+
+        if not isinstance(content, str):
+            raise ValueError("content must be a string")
+
+        if os.path.exists(filepath):
+            print(f"File already exists: {filepath}")
+            return
+
+        with open(filepath, 'w', encoding='utf-8') as file:
+            file.write(content)
+
+    @staticmethod
+    def get_filename_from_url(url: str, filetype: str = ".md") -> str:
+        """
+        Gets the filename from the URL (the ending)
+        """
+        if not isinstance(url, str):
+            raise ValueError("url must be a string")
+
+        if not isinstance(filetype, str):
+            raise ValueError("filetype must be a string")
+
+        if not filetype.startswith("."):
+            filetype = f".{filetype}"
+
+        return url.split("/")[-1] + filetype
+
+    def get_post_content(self, url: str) -> str:
+        raise NotImplementedError
+
+    def scrape_all_posts(self, only_scrape_n_posts: int= 0) -> None:
+        """
+        Iterates over all posts and saves them as markdown files
+        """
+        count = 0
+        for url in tqdm(self.post_urls):
+            try:
+                filename = self.get_filename_from_url(url, filetype=".md")
+                filepath = os.path.join(self.save_dir, filename)
+                if not os.path.exists(filepath):
+                    content = self.get_post_content(url)
+                    md = self.html_to_md(content)
+                    self.save_to_file(filepath, md)
+                else:
+                    print(f"File already exists: {filepath}")
+            except Exception as e:
+                print(f"Error scraping post: {e}")
+            count+= 1
+            if only_scrape_n_posts != 0 and count == only_scrape_n_posts:
+                break
+
+class SubstackScraper(BaseSubstackScraper):
+    def __init__(self, base_substack_url: str, savdir: str):
+        super().__init__(base_substack_url, savdir)
+
+    def get_post_content(self, url: str) -> str:
+        """
+        Gets post content using requests and soup and returns it as a string
         """
         try:
             page = requests.get(url, headers=None)
             soup = BeautifulSoup(page.content, "html.parser")
-            text = html2text.html2text(str(soup.select_one("div.available-content")))  # This prints it more nicely than bs4
-            _, metadata = self._substack_reader(soup, url)  # Note: _ here might actually be more parseable than the text above! But just leaving as is for now
-            print(text)
-            print(metadata)
+            content = str(soup.select_one("div.available-content"))
+            return content
         except Exception as e:
             raise ValueError(f"Error fetching page: {e}") from e
 
 
-# This can inherit from SubstackScraper down the line, but just keep it separate for now
-# Try log in using selenium and then scrape the premium content
-class PremiumSubstackScraper(SubstackScraper):
-    def __init__(self, base_url: str, headless: bool = False):
-        super().__init__(base_url)
+class PremiumSubstackScraper(BaseSubstackScraper):
+    def __init__(self, base_substack_url: str, savdir: str, headless: bool = False):
+        super().__init__(base_substack_url, savdir)
 
         options = EdgeOptions()
         if headless:
@@ -81,9 +150,8 @@ class PremiumSubstackScraper(SubstackScraper):
 
         self.driver = webdriver.Edge(EdgeChromiumDriverManager().install(), options=options)
         self.login()
-        self.post_urls: List[str] = self.get_all_post_urls()
 
-    def login(self) -> None:  # sourcery skip: extract-duplicate-method
+    def login(self) -> None:   # sourcery skip: extract-duplicate-method
         """
         This method logs into Substack using Selenium
         """
@@ -107,36 +175,23 @@ class PremiumSubstackScraper(SubstackScraper):
         submit.click()
         sleep(5) # Wait for the page to load
 
-
-    def save_posts_as_html(self, data_dir: str) -> None:
+    def get_post_content(self, url: str) -> str:
+        # sourcery skip: inline-immediately-returned-variable
         """
-        This method loops over the post URLs and saves the content as an HTML file.
-        It names the HTML file after the last part of the URL.
+        Gets post content using requests and soup and returns it as a string
         """
-        count = 0
-        if not os.path.exists(data_dir):  # ensure the directory exists
-            os.makedirs(data_dir)
-        for url in tqdm(self.post_urls, desc="Saving posts", unit="post"):
-            try:
-                file_name = url.rsplit('/', 1)[-1]  # get the last part of the URL
-                file_path = os.path.join(data_dir, f"{file_name}.html")
-                if not os.path.exists(file_path):  # only proceed if the file does not already exist
-                    self.driver.get(url)
-                    sleep(5)  # wait for the page to load
-                    page_content = self.driver.page_source
-                    with open(file_path, "w", encoding="utf-8") as f:
-                        f.write(page_content)
-                else:
-                    print(f"File {file_path} already exists. Skipping.")
-            except Exception as e:
-                print(f"Error fetching or saving page: {e}")
-            count += 1
-
+        try:
+            self.driver.get(url)
+            soup = BeautifulSoup(self.driver.page_source, "html.parser")
+            content = str(soup.select_one("div.available-content"))
+            return content
+        except Exception as e:
+            raise ValueError(f"Error fetching page: {e}") from e
 
 
 def main():
-    premium_substack_scraper = PremiumSubstackScraper("https://ava.substack.com/", headless=True)
-    premium_substack_scraper.save_posts_as_html("data/ava")
+    premium_scraper = PremiumSubstackScraper(base_substack_url="https://ava.substack.com/", savdir="data/ava_test")
+    premium_scraper.scrape_all_posts(only_scrape_n_posts=5)
 
 
 if __name__ == "__main__":
