39a593c5038c927b9dfd2f063678d9f752d444cc
timf34 <farrelti@tcd.ie>
2024-01-28 02:44:04 +0000

    added sorting functionality

--- assets/js/populate-essays.js
+++ assets/js/populate-essays.js
@@ -1,3 +1,19 @@
+let sortLikesAscending = false;
+let sortDatesAscending = false;
+
+function sortEssaysByDate(data) {
+    sortDatesAscending = !sortDatesAscending;  // Toggle the sort order
+    return data.sort((a, b) => sortDatesAscending
+        ? new Date(a.date) - new Date(b.date)
+        : new Date(b.date) - new Date(a.date));
+}
+
+function sortEssaysByLikes(data) {
+    sortLikesAscending = !sortLikesAscending;  // Toggle the sort order
+    return data.sort((a, b) => sortLikesAscending
+        ? a.like_count - b.like_count
+        : b.like_count - a.like_count);
+}
 function populateEssays(data) {
     const essaysContainer = document.getElementById('essays-container');
     const list = data.map(essay => `
@@ -10,6 +26,15 @@ function populateEssays(data) {
 
 document.addEventListener('DOMContentLoaded', () => {
     const embeddedDataElement = document.getElementById('essaysData');
-    const essaysData = JSON.parse(embeddedDataElement.textContent);
+    let essaysData = JSON.parse(embeddedDataElement.textContent);
+
+    document.getElementById('sort-by-date').addEventListener('click', () => {
+        populateEssays(sortEssaysByDate([...essaysData]));
+    });
+
+    document.getElementById('sort-by-likes').addEventListener('click', () => {
+        populateEssays(sortEssaysByLikes([...essaysData]));
+    });
+
     populateEssays(essaysData);
 });
