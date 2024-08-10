ca0d67fdac1a151e67a1f0d3b4f1fdb04918a7e5
timf34 <farrelti@tcd.ie>
2024-08-10 20:52:17 +0100

    backwards compat: changed BASE_HTML_DIR to old name #10

--- substack_scraper.py
+++ substack_scraper.py
@@ -23,7 +23,7 @@ from config import EMAIL, PASSWORD
 USE_PREMIUM: bool = False  # Set to True if you want to login to Substack and convert paid for posts
 BASE_SUBSTACK_URL: str = "https://www.afterbabel.com/"  # Substack you want to convert to markdown
 BASE_MD_DIR: str = "substack_md_files"  # Name of the directory we'll save the .md essay files
-BASE_HTML_DIR: str = "substack_html_files"  # Name of the directory we'll save the .html essay files
+BASE_HTML_DIR: str = "substack_html_pages"  # Name of the directory we'll save the .html essay files
 HTML_TEMPLATE: str = "author_template.html"  # HTML template to use for the author page
 JSON_DATA_DIR: str = "data"
 NUM_POSTS_TO_SCRAPE: int = 3  # Set to 0 if you want all posts
